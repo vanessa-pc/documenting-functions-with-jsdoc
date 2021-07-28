48727a47f7188be5fda3567a641e9967618715aa
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-07-28 18:58:06 +0300

    Add JSDoc and code

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,14 @@
 // TODO (Ex.0): document and write tests for this function
 
+/**
+ * Return the negative version of given number
+ * @param {number} n - input number
+ * @returns {number} the negative version of input number 
+ */
 function asNegative(n) {
-  // ...
+  if (n>0){
+    return -n
+  } else{
+    return n
+  }
 }
-
-
